e2bf668dbcf360ad2af43825464fa85a2fc96a5a
Iwo Plaza <iwoplaza@gmail.com>
2025-06-20 18:53:37 +0200

    Prettier example

--- apps/jotai-gpu-docs/src/pages/examples/counter/CounterApp.tsx
+++ apps/jotai-gpu-docs/src/pages/examples/counter/CounterApp.tsx
@@ -1,8 +1,10 @@
-import { type Atom, atom, useAtomValue, useSetAtom } from 'jotai';
+import { atom, useSetAtom } from 'jotai';
 import { gpuAtom, withUpload } from 'jotai-gpu';
-import { Suspense, useCallback } from 'react';
+import { useCallback } from 'react';
 import tgpu from 'typegpu';
 import * as d from 'typegpu/data';
+import { Arrow } from './Arrow';
+import { AtomBox } from './AtomBox';
 tgpu;
 
 const counterAtom = withUpload(d.f32, atom(1));
@@ -16,26 +18,6 @@ const quadAtom = atom(async (get) => {
 	return (await get(doubleAtom)) * 2;
 });
 
-export function CountDisplay(props: {
-	countAtom: Atom<number> | Atom<Promise<number>>;
-}) {
-	const counter = useAtomValue(props.countAtom);
-
-	return (
-		<span className="flex justify-center items-center w-8 h-8 bg-slate-200">
-			{counter}
-		</span>
-	);
-}
-
-export function Fallback() {
-	return (
-		<span className="flex justify-center items-center w-8 h-8 bg-slate-200">
-			🌀
-		</span>
-	);
-}
-
 export default function CounterApp() {
 	const setCounter = useSetAtom(counterAtom);
 
@@ -45,39 +27,53 @@ export default function CounterApp() {
 
 	return (
 		<div>
-			<h1 className="text-center text-xl">Jotai GPU - Compute Graph Example</h1>
+			<h1 className="text-center text-xl my-8">Jotai GPU - Compute Graph Example</h1>
 			<button
 				type="button"
 				onClick={increment}
-				className="px-4 py-2 block mx-auto my-4 bg-slate-100 hover:bg-slate-200 rounded-md"
+				className="px-4 py-2 block mx-auto my-4 bg-slate-700 hover:bg-slate-600 active:bg-slate-500 transition-colors duration-75 rounded-md"
 			>
-				Increment
+				Increment counter
 			</button>
-			<div className="flex">
-				<section className="min-w-3xs flex flex-col items-center">
-					<h2 className="text-slate-600 mb-4">CPU</h2>
-					<Suspense fallback={<Fallback />}>
-						<p className="flex items-center gap-1">
-							counter: <CountDisplay countAtom={counterAtom} />
-						</p>
-					</Suspense>
-					<div className="h-20" />
-					<p className="flex items-center gap-1">
-						quad:
-						<Suspense fallback={<Fallback />}>
-							<CountDisplay countAtom={quadAtom} />
-						</Suspense>
-					</p>
+			<div className="mx-auto w-fit grid grid-cols-[minmax(0,1fr)_auto_minmax(0,1fr)]">
+				<section className="flex flex-col items-end">
+					<h2 className="text-slate-600 mb-4 self-center">Computed on the <strong>CPU</strong></h2>
+					<div className="relative">
+						<AtomBox
+							valueAtom={counterAtom}
+							codeHtml={`\
+const counterAtom = withUpload(d.f32, atom(1));`}
+						/>
+						<div className="absolute -right-10 top-5">
+							<Arrow x={60} y={60} />
+						</div>
+					</div>
+					<div className="h-32" />
+					<AtomBox
+						valueAtom={quadAtom}
+						codeHtml={`\
+const quadAtom = atom(async (get) => {
+	return (await get(doubleAtom)) * 2;
+});`}
+					/>
 				</section>
-				<section className="min-w-3xs flex flex-col items-center">
-					<h2 className="text-slate-600 mb-4">GPU</h2>
-					<div className="h-14" />
-					<p className="flex items-center gap-1">
-						double:
-						<Suspense fallback={<Fallback />}>
-							<CountDisplay countAtom={doubleAtom} />
-						</Suspense>
-					</p>
+				<div className='mx-16 self-stretch border-l border-dashed opacity-20 -z-10' />
+				<section className="flex flex-col items-start">
+					<h2 className="text-slate-600 mb-4 self-center">Computed on the <strong>GPU</strong></h2>
+					<div className="h-24" />
+					<div className="relative">
+						<AtomBox
+							valueAtom={doubleAtom}
+							codeHtml={`\
+const doubleAtom = gpuAtom(d.f32)(() => {
+	'kernel';
+	return counterAtom.$ * 2;
+});`}
+						/>
+						<div className="absolute -left-6 top-20">
+							<Arrow x={-60} y={60} />
+						</div>
+					</div>
 				</section>
 			</div>
 		</div>
