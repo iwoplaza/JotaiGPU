347e3c1d9e24def941137cdfd53b30eea738b5b4
Iwo Plaza <iwoplaza@gmail.com>
2025-06-20 21:32:46 +0200

    Twoslash on the homepage

---
+++ apps/jotai-gpu-docs/ec.config.mjs
@@ -0,0 +1,17 @@
+import ecTwoSlash from 'expressive-code-twoslash';
+import ts from 'typescript';
+
+/** @type {import('@astrojs/starlight/expressive-code').StarlightExpressiveCodeOptions} */
+export default {
+  plugins: [
+    ecTwoSlash({
+      twoslashOptions: {
+        strict: true,
+        compilerOptions: {
+          moduleResolution: ts.ModuleResolutionKind.Bundler,
+          lib: ['ESNext'],
+        },
+      },
+    }),
+  ],
+};
